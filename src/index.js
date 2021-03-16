8e1424b50f1b261d977945e7bc7db69e6c5a724d
Youichii <baurelien@hotmail.com>
2021-03-16 22:47:11 +0100

    adding informations html

--- src/index.js
+++ src/index.js
@@ -8,10 +8,13 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root')  //réference a index.html <div id='root'> dnas le body
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+//entry point for React
