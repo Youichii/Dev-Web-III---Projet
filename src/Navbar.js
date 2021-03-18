945c2ee54d4f4fd389d02e723c4cc86ae0a45bd6
clementinesacre <sacre.clementine@gmail.com>
2021-03-18 19:30:05 +0100

    Traduction de la navbar en js + son css

---
+++ src/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <nav id="baniere" className="baniere">
+                <ul>
+                    <li><a href="page1.html">Accueil</a></li>
+                    <li><a href="page1.html">Informations</a></li>
+                    <li><img src="img/logo.jpg" href="page1.html"/></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Accueil</a></li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default Navbar;
