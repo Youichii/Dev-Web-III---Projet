e8eb2049c0bf678d4b0d3c6d5c364cf08b19e90d
clementinesacre <sacre.clementine@gmail.com>
2021-03-20 10:58:59 +0100

    Mise en place du js traduit en react panier

--- src/Panier.js
+++ src/Panier.js
@@ -1,22 +1,46 @@
+import {useState} from "react" ;
+
 const Panier = () => {
 
     const [aliments, setAliments] = useState([{"id" : "A", "nom" : "poulet", "quantite" : 2, "prix_unite" : 5}, 
                                             {"id" : "B", "nom" : "viande", "quantite" : 7, "prix_unite" : 8}, 
                                             {"id" : "C", "nom" : "clem", "quantite" : 9, "prix_unite" : 260}]);
 
+    const [heures, setHeures] = useState([]);
+
+    const init_heures = () => {
+        const intermediaire = [] ;
+        let min = 1080 ; //18h
+        let max = 1440 ; //00h
+
+        for (var i = min; i < max; i+=15) {
+            var minutes = i ;
+            var nbHour = parseInt(minutes / 60);
+            var nbminuteRestante = (minutes % 60);
+            if(nbminuteRestante == 0){   
+                intermediaire.push(({"h" : nbHour.toString(), "m" : "00"}));
+            }
+            else{
+                intermediaire.push(({"h" : nbHour.toString(), "m" : nbminuteRestante.toString()}));
+            }
+        }
+        console.log(intermediaire);
+        setHeures(intermediaire);
+    }
+
     const changer_prix = () => {
-        console.log('hello ninjas', e);
+        console.log('hello ninjas');
     }
 
     return (
-        <div className="panier">
+        <div className="panier" onLoad={init_heures}>
             
             <div className="row">
                 <div className="col-7 col-s-3">
                     <div id="menu">
                         <table id="titre_aliments" width="800px">
                             <tr>
-                                <td width="223px">Produit</td>
+                                <td width="223px" onClick={init_heures}>Produit</td>
                                 <td width="223px" id="titre_qtite">Quantité</td>
                                 <td width="223px">Prix</td>
                             </tr>
@@ -47,26 +71,30 @@ const Panier = () => {
                                 <li>
                                     <label for="heure_livraison">Heure</label><br />
                                     <div id="heure_livraison">
-
+                                        <select name="heures_reserv" id="heures_reserv">
+                                            {heures.map(heure => (
+                                                <option value={heure.h, ":", heure.m}>{heure.h}{":"}{heure.m}</option>
+                                            ))}
+                                        </select>
                                     </div>
                                 </li>
                             </div>
 
-                            <div class="elem_liste">
+                            <div className="elem_liste">
                                 <li>
                                     <label for="adresse_livraison">Adresse de livraison</label><br />
                                     <input id="adresse_livraison" name="add_livraison" type="text" />
                                 </li>
                             </div>
                             
-                            <div class="elem_liste">
+                            <div className="elem_liste">
                                 <li>
                                     <label for="moyen_reception">Moyen de réception</label><br />
                                     <input id="moyen_reception" name="moy_reception" type="text" />
                                 </li>
                             </div>
                             
-                            <div class="elem_liste">
+                            <div className="elem_liste">
                                 <li>
                                     <label for="mode_payement">Mode de payement</label><br />
                                     <input id="mode_payement" name="moy_payement" type="text" />
@@ -76,16 +104,16 @@ const Panier = () => {
                     </div>
                 </div>
 
-                <div class="col-6 col-s-9" id="elem_commentaire">
+                <div className="col-6 col-s-9" id="elem_commentaire">
                     <label for="commentaire" id="titre_elem_commentaire">Commentaire</label><br />
                     <input id="commentaire" name="com" type="text" />
                 </div>
 
-                <div class="col-2 col-s-9" id="elem_bouton_envoyer">
+                <div className="col-2 col-s-9" id="elem_bouton_envoyer">
                     <input id="bouton_envoyer" name="bout_envoyer" type="button" value="Envoyer" />
                 </div>
                 
-                <div class="col-2 col-s-9" id="elem_bouton_annuler">
+                <div className="col-2 col-s-9" id="elem_bouton_annuler">
                     <input id="bouton_annuler" name="bout_annuler" type="button" value="Annuler" />
                 </div>
 
