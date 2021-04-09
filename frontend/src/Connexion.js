f82c20330a3a46afdbc0db92c6cea986b1f7c625
clementinesacre <sacre.clementine@gmail.com>
2021-04-09 17:06:14 +0200

    Mise en place grid pour responsive

--- frontend/src/Connexion.js
+++ frontend/src/Connexion.js
@@ -1,36 +1,25 @@
 const Connexion = () => {
     return (
-        <div className="connexion">
-            <div id="cadre_connexion">
-                <div id="boutons_choix">
-                    <span id="bouton_connexion">SE CONNECTER</span>
-                    <span id="bouton_inscription"><a href="inscription.html">S'INSCRIRE</a></span>
-                </div>
-                
-                <div>
-                    <div id="titre_connexion">CONNEXION AVEC UNE ADRESSE E-MAIL</div>
-                    
-                    <form>
-                        <div id="user">
-                            <div class="div_user">ADRESSE E-MAIL</div>
-                            <div class="div_text_user">
-                                <input id="text_user" type="text" required maxlength="100" />
-                            </div>
-                        </div>
-                        
-                        <div id="pwd">
-                            <div class="div_user">MOT DE PASSE</div>
-                            <div class="div_text_user"><input id="text_mdp" type="password" required maxlength="100" /></div>
-                        </div>
-                        
-                        <div id="connexion_envoi">
-                            <input id="bouton_connexion_envoi" type="submit" value="CONNEXION"/>
-                        </div>
-                    </form>
-                    
-                    <div id ="pwd_oubli"><div>Mot de passe oublié ?</div></div>
-                </div>
+        <div className="connexion c_cadre">
+            <div id="cadre_connexion" className="i_info_connexion c_info_connexion">
+                <div className="i_bouton_connexion">SE CONNECTER</div>
+				<div className="i_bouton_inscription">S'INSCRIRE</div>
+				<div className="i_titre_connexion">CONNEXION AVEC UNE ADRESSE E-MAIL</div>
 
+                <div class="i_zones_info c_zones_info">
+					<div class="i_titre_adresse">ADRESSE E-MAIL</div>
+					<div class="i_champ_adresse">
+						<input id="text_user" type="text" required maxlength="100" />
+					</div>
+					<div class="i_titre_mdp">MOT DE PASSE</div>
+					<div class="i_champ_mdp">
+						<input id="text_mdp" type="password" required maxlength="100" />
+					</div>
+					<div class="i_bouton_envoi">
+						<input id="bouton_connexion_envoi" type="submit" value="CONNEXION" />
+					</div>
+					<div class="i_mdp_oubli" id ="pwd_oubli">Mot de passe oublié ?</div>
+				</div>
             </div>		
         </div>
     );
