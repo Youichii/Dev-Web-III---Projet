8349ca4ccfe9e28c025d5a7f1cc1ed9bc9bf52f4
clementinesacre <sacre.clementine@gmail.com>
2021-04-10 10:45:29 +0200

    Mise en place d'un grid sur la page inscirption

--- frontend/src/Inscription.js
+++ frontend/src/Inscription.js
@@ -1,101 +1,72 @@
 const Inscription = () => {
     return (
-        <div className="inscription">
-            <div id="cadre_connexion">
-                <div id="boutons_choix">
-                    <span id="bouton_connexion"><a href="connexion.html">SE CONNECTER</a></span>
-                    <span id="bouton_inscription">S'INSCRIRE</span>
-                </div>
-                
-                <div>
-                    <div id="titre_connexion">CONNEXION AVEC UNE ADRESSE E-MAIL</div>
-                    
-                    <form>
-                        <div>
-                            <div class="div_infos">NOM</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="nom_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">PRENOM</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="prenom_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">ADRESSE</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="adresse_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">DATE DE NAISSANCE</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="naissance_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">TELEPHONE</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="telephone_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">ADRESSE E-MAIL</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="mail_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">SEXE</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="sexe_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">MOT DE PASSE</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="mdp_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">NEWSLETTER</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="newsletter_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">POLITIQUE</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="politique_user" required />
-                            </div>
-                        </div>
-                        
-                        <div>
-                            <div class="div_infos">CAPTCHA</div>
-                            <div class="div_text_infos">
-                                <input type="texte" id="captcha_user" required />
-                            </div>
-                        </div>
-                        
-                        <div id="connexion_envoi">
-                            <input id="bouton_connexion_envoi" type="submit" value="CONNEXION" />
-                        </div>
-                    </form>
-                    
-                </div>
-
-            </div>	
+        <div className="inscription grid_container">
+            <div id="cadre_inscription" className="item1 grid_container2">			
+				<div className="i_bouton_con">CONNEXION</div>
+				<div className="i_bouton_insc">INSCRIPTION</div>
+				<div className="i_titre_connexion">CONNEXION AVEC UNE ADRESSE E-MAIL</div>
+				
+				<div className="grid_container3 item2">
+					<div className="item3 pol">NOM</div>
+					<div className="item4">
+						<input type="texte" id="nom_user" required maxlength="100" />
+					</div>
+					
+					<div className="item5 pol">PRENOM</div>
+					<div className="item6">
+						<input type="texte" id="prenom_user" required />
+					</div>
+				
+					<div className="item7 pol">ADRESSE</div>
+					<div className="item8">
+						<input type="texte" id="adresse_user" required />
+					</div>
+				
+					<div className="item9 pol">DATE DE NAISSANCE</div>
+					<div className="div_text_infos item10">
+						<input type="texte" id="naissance_user" required />
+					</div>
+					
+					<div className="item15 pol">SEXE</div>
+					<div className="item16">
+						<input type="texte" id="sexe_user" required />
+					</div>
+				
+					<div className="item11 pol">TELEPHONE</div>
+					<div className="item12">
+						<input type="texte" id="telephone_user" required />
+					</div>
+				
+					<div className="item13 pol">ADRESSE E-MAIL</div>
+					<div className="item14">
+						<input type="texte" id="mail_user" required />
+					</div>
+				
+					<div className="item17 pol">MOT DE PASSE</div>
+					<div className="item18">
+						<input type="texte" id="mdp_user" required />
+					</div>
+				
+					<div className="item19 pol">NEWSLETTER</div>
+					<div className="item20">
+						<input type="texte" id="newsletter_user" required />
+					</div>
+				
+					<div className="item21 pol">POLITIQUE</div>
+					<div className="item22">
+						<input type="texte" id="politique_user" required />
+					</div>
+				
+					<div className="item23 pol">CAPTCHA</div>
+					<div className="item24">
+						<input type="texte" id="captcha_user" required />
+					</div>
+					
+					<div id="connexion_envoi" className="item25">
+						<input id="bouton_connexion_envoi" type="submit" value="INSCRIPTION" onclick="inscription();" />
+					</div>
+				</div>
+			</div>
         </div>
     );
 }
