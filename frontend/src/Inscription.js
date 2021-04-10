64c4ce0d62417a08cc2e1dd63cba489adb85c9ef
clementinesacre <sacre.clementine@gmail.com>
2021-04-10 10:57:34 +0200

    Renommage des classes

--- frontend/src/Inscription.js
+++ frontend/src/Inscription.js
@@ -1,68 +1,68 @@
 const Inscription = () => {
     return (
-        <div className="inscription grid_container">
-            <div id="cadre_inscription" className="item1 grid_container2">			
+        <div className="inscription c_cadre_inscription">
+            <div id="cadre_inscription" class="i_info_inscription c_info_inscription">			
 				<div className="i_bouton_con">CONNEXION</div>
 				<div className="i_bouton_insc">INSCRIPTION</div>
 				<div className="i_titre_connexion">CONNEXION AVEC UNE ADRESSE E-MAIL</div>
 				
-				<div className="grid_container3 item2">
-					<div className="item3 pol">NOM</div>
-					<div className="item4">
+				<div className="c_champs_informations i_champs_informations">
+					<div className="i_titre_nom pol">NOM</div>
+					<div className="i_champ_nom">
 						<input type="texte" id="nom_user" required maxlength="100" />
 					</div>
 					
-					<div className="item5 pol">PRENOM</div>
-					<div className="item6">
+					<div className="i_titre_prenom pol">PRENOM</div>
+					<div className="i_champ_prenom">
 						<input type="texte" id="prenom_user" required />
 					</div>
 				
-					<div className="item7 pol">ADRESSE</div>
-					<div className="item8">
+					<div className="i_titre_adresse_insc pol">ADRESSE</div>
+					<div className="i_champ_adresse_insc">
 						<input type="texte" id="adresse_user" required />
 					</div>
 				
-					<div className="item9 pol">DATE DE NAISSANCE</div>
-					<div className="div_text_infos item10">
+					<div className="i_titre_anniversaire pol">DATE DE NAISSANCE</div>
+					<div className="i_champ_anniversaire">
 						<input type="texte" id="naissance_user" required />
 					</div>
 					
-					<div className="item15 pol">SEXE</div>
-					<div className="item16">
+					<div className="i_titre_sexe pol">SEXE</div>
+					<div className="i_champ_sexe">
 						<input type="texte" id="sexe_user" required />
 					</div>
 				
-					<div className="item11 pol">TELEPHONE</div>
-					<div className="item12">
+					<div className="i_titre_telephone pol">TELEPHONE</div>
+					<div className="i_champ_telephone">
 						<input type="texte" id="telephone_user" required />
 					</div>
 				
-					<div className="item13 pol">ADRESSE E-MAIL</div>
-					<div className="item14">
+					<div className="i_titre_mail pol">ADRESSE E-MAIL</div>
+					<div className="i_champ_mail">
 						<input type="texte" id="mail_user" required />
 					</div>
 				
-					<div className="item17 pol">MOT DE PASSE</div>
-					<div className="item18">
+					<div className="i_titre_mdp_insc pol">MOT DE PASSE</div>
+					<div className="i_champ_mdp_insc">
 						<input type="texte" id="mdp_user" required />
 					</div>
 				
-					<div className="item19 pol">NEWSLETTER</div>
-					<div className="item20">
+					<div className="i_titre_newsletter pol">NEWSLETTER</div>
+					<div className="i_champ_newsletter">
 						<input type="texte" id="newsletter_user" required />
 					</div>
 				
-					<div className="item21 pol">POLITIQUE</div>
-					<div className="item22">
+					<div className="i_titre_politique pol">POLITIQUE</div>
+					<div className="i_champ_politique">
 						<input type="texte" id="politique_user" required />
 					</div>
 				
-					<div className="item23 pol">CAPTCHA</div>
-					<div className="item24">
+					<div className="i_titre_captcha pol">CAPTCHA</div>
+					<div className="i_champ_captcha">
 						<input type="texte" id="captcha_user" required />
 					</div>
 					
-					<div id="connexion_envoi" className="item25">
+					<div id="connexion_envoi" className="i_bouton_envoi_insc">
 						<input id="bouton_connexion_envoi" type="submit" value="INSCRIPTION" onclick="inscription();" />
 					</div>
 				</div>
