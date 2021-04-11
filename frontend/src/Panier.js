3395e8b3776ea35af39de3d9d8aaf5fd12ea0091
clementinesacre <sacre.clementine@gmail.com>
2021-04-11 18:53:28 +0200

    Ajout d'un grid sur la page panier pour responsive

--- frontend/src/Panier.js
+++ frontend/src/Panier.js
@@ -34,90 +34,67 @@ const Panier = () => {
 
     return (
         <div className="panier" onLoad={init_heures}>
-            
-            <div className="row">
-                <div className="col-7 col-s-3">
-                    <div id="menu">
-                        <table id="titre_aliments" width="800px">
-                            <tr>
-                                <td width="223px" onClick={init_heures}>Produit</td>
-                                <td width="223px" id="titre_qtite">Quantité</td>
-                                <td width="223px">Prix</td>
-                            </tr>
-                        </table>
 
-                        <div id="info_aliments">
-                            <table id="aliments">
-                                {aliments.map(element => (
-                                    <tr>
-                                        <td width="290px" className="case_aliments">{element.nom}</td>
-                                        <td className="case_aliment qtite_aliment">
-                                            <input className="nombre_aliment" type="number" value={element.quantite} onChange={changer_prix(this, element.id)} min="0" max="100" />
-                                        </td>
-                                        <td id={element.id} className="case_aliment" width="250px">{element.prix_unite}*{element.quantite}{"€"}</td>
-                                    </tr>
-                                ))}
-                            </table>
-                        </div>
-                        
-                        <div id="total_prix"></div>
-                    </div>
-                </div>
+            <div className="c_info_panier i_info_panier">
+				<div className="i_titre_produit" onClick={init_heures}>Produit</div>
+				<div className="i_titre_quantite" id="titre_qtite">Quantité</div>
+				<div className="i_titre_prix">Prix</div>
+			
+				<div className="i_info_aliments c_info_aliments" id="info_aliments">
+                    {aliments.map(element => (
+                        <tr>
+                            <td width="290px" className="case_aliments">{element.nom}</td>
+                            <td className="case_aliment qtite_aliment">
+                                <input className="nombre_aliment" type="number" value={element.quantite} onChange={changer_prix(this, element.id)} min="0" max="100" />
+                            </td>
+                            <td id={element.id} className="case_aliment" width="250px">{element.prix_unite}*{element.quantite}{"€"}</td>
+                        </tr>
+                    ))}
+				</div>
+			</div>
 
-                <div className="col-4 col-s-9">
-                    <div id="informations">
-                        <ul>
-                            <div className="elem_liste" id="elem_heure">
-                                <li>
-                                    <label for="heure_livraison">Heure</label><br />
-                                    <div id="heure_livraison">
-                                        <select name="heures_reserv" id="heures_reserv">
-                                            {heures.map(heure => (
-                                                <option value={heure.h, ":", heure.m}>{heure.h}{":"}{heure.m}</option>
-                                            ))}
-                                        </select>
-                                    </div>
-                                </li>
-                            </div>
+            <div className="c_info_reception i_info_reception">
+				<div className="i_heure" id="elem_heure">
+					<label for="heure_livraison">Heure</label><br />
+					<div id="heure_livraison">
+                        <select name="heures_reserv" id="heures_reserv">
+                            {heures.map(heure => (
+                                <option value={heure.h, ":", heure.m}>{heure.h}{":"}{heure.m}</option>
+                            ))}
+                        </select>
+                    </div>								
+				</div>
 
-                            <div className="elem_liste">
-                                <li>
-                                    <label for="adresse_livraison">Adresse de livraison</label><br />
-                                    <input id="adresse_livraison" name="add_livraison" type="text" />
-                                </li>
-                            </div>
-                            
-                            <div className="elem_liste">
-                                <li>
-                                    <label for="moyen_reception">Moyen de réception</label><br />
-                                    <input id="moyen_reception" name="moy_reception" type="text" />
-                                </li>
-                            </div>
-                            
-                            <div className="elem_liste">
-                                <li>
-                                    <label for="mode_payement">Mode de payement</label><br />
-                                    <input id="mode_payement" name="moy_payement" type="text" />
-                                </li>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
+				<div className="i_moy_reception">
+					<label for="moyen_reception">Moyen de réception</label><br />
+					<input id="moyen_reception" name="moy_reception" type="text" />
+				</div>
+				
+				<div className="i_payement">
+					<label for="mode_payement">Mode de payement</label><br />
+					<input id="mode_payement" name="moy_payement" type="text" />
+				</div>
+				
+				<div className="i_adresse">
+					<label for="adresse_livraison">Adresse de livraison</label><br />
+					<input id="adresse_livraison" name="add_livraison" type="text" />
+				</div>
+			</div>
 
-                <div className="col-6 col-s-9" id="elem_commentaire">
-                    <label for="commentaire" id="titre_elem_commentaire">Commentaire</label><br />
-                    <input id="commentaire" name="com" type="text" />
-                </div>
+            <div className="i_commentaire" id="elem_commentaire">
+				<label for="commentaire" id="titre_elem_commentaire">Commentaire</label><br />
+				<input id="commentaire" name="com" type="text" />
+			</div>
 
-                <div className="col-2 col-s-9" id="elem_bouton_envoyer">
-                    <input id="bouton_envoyer" name="bout_envoyer" type="button" value="Envoyer" />
-                </div>
-                
-                <div className="col-2 col-s-9" id="elem_bouton_annuler">
-                    <input id="bouton_annuler" name="bout_annuler" type="button" value="Annuler" />
-                </div>
-
-            </div>
+			<div className="i_boutons c_boutons">
+				<div className="i_bouton_envoyer" id="elem_bouton_envoyer">
+					<input id="bouton_envoyer" name="bout_envoyer" type="button" value="Envoyer" />
+				</div>
+				
+				<div className="i_bouton_annuler" id="elem_bouton_annuler">
+					<input id="bouton_annuler" name="bout_annuler" type="button" value="Annuler" />
+				</div>
+			</div>
 
         </div>
     );
