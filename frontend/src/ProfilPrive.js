ce957ca2b19bf84528ccc6819c0f8b88bc534600
Youichii <baurelien@hotmail.com>
2021-04-20 18:32:32 +0200

    fixing css with for container with private infos
    bug where the height was cut if screen too wide

--- frontend/src/ProfilPrive.js
+++ frontend/src/ProfilPrive.js
@@ -34,14 +34,20 @@ const ProfilPrive = () => {
                 <div className="gauche">
                     <label>*Your username*</label><br />
                     <input type="text" name="Username" maxLength="10" placeholder="Changer le pseudo" onChange="" />
-                    <p>Aurélien Brille</p>                    
+                    <button onClick=" ">Modifier</button>
+                    <p>{cecile}</p>                    
                     <p>Clos de la Croisette 5 <br /> 1325 Chaumont Gistoux</p>
-                    <input type="text" name="Username" maxLength="" placeholder="Changer l'adresse" onChange="" />
+                    <input type="text" name="Username" maxLength="50" placeholder="Rue" onChange="" /><br/>
+                    <input type="text" name="Username" maxLength="10" placeholder="Numéro" onChange="" /><br/>
+                    <input type="text" name="Username" maxLength="6" placeholder="Code Postal" onChange="" /><br/>
+                    <input type="text" name="Username" maxLength="40" placeholder="Ville" onChange="" /><br/>
+                    <button onClick=" ">Modifier</button>
                     <p>0495757504</p>
                     <input type="text" name="Username" maxLength="20" placeholder="Changer le numéro de téléphone" onChange="" />
+                    <button onClick=" ">Modifier</button>
                     <p>baurelien@hotmail.com</p>
-                    <input type="text" name="Username" maxLength="10" placeholder="Changer l'adresse mail" onChange="" /><br />
-                    <input type="text" name="Username" maxLength="50" placeholder="Changer username test" onChange= {(e) => {
+                    <input type="text" name="Username" maxLength="50" placeholder="Changer l'adresse mail" onChange="" /><br />
+                    <input type="text" name="Username" maxLength="10" placeholder="Changer username test" onChange= {(e) => {
                         setUsername(e.target.value)
                     }}/><br />
                     <button onClick={submitUsername}>Username</button>
