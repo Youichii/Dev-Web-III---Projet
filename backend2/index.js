6d8d228136fce4d16cb8ca079802236e55bbfcc3
clementinesacre <sacre.clementine@gmail.com>
2021-04-20 09:42:52 +0200

    Correction erreur node.js backend

---
+++ backend2/index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const mysql = require('mysql')
+const cors = require('cors')
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: "Stegosaure915",
+  database : 'profilprive'
+})
+
+app.listen(3001, () => {
+  console.log("running on port 3001");
+})
+
+app.use(express.json())
+app.use(cors())  //to avoid CORS policy
+
+app.get('/', (req,res) => {
+  res.send("hello");
+})
+
+app.post('/api/insert', (req, res) => {
+
+  const username = req.body.Username  //to take the variable from the html page
+  
+  const sqlInsert = "INSERT INTO `client`(`Username`) VALUES (?)"
+  db.query(sqlInsert, [username], (err, result) => {
+    console.log(err)
+  })
+})
