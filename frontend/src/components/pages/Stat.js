3ad7129d92039dbe076fb6f2192b57a007f76564
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-01 12:30:22 +0200

    arrangement des dossier

---
+++ frontend/src/components/pages/Stat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../App.css';
+import Chart from '../../../../Statistique/src/components/Chart'
+
+export default function Stat() {
+  return (
+    <>
+        <Chart />
+    </>
+    
+  );
+
+};
