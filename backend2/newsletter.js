7c0cbed201efdb48600fdc1bfb4917dc6122235f
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-01 20:58:15 +0200

    ajout de modules

---
+++ backend2/newsletter.js
@@ -0,0 +1,16 @@
+//import App from "./client/src/App"
+//const {getEmailData} = require("./mail");
+//const {sendEmail} = require("./mail")
+const Hello = () => {
+
+    return (`
+     
+            <h1 style="color:white"> Bonjour </h1>
+            <p style="color:white"> j'espère que tu vas bien ! Aujourd'hui au Chick'N'Fish ca va super<br>
+            car nous avons une super promo pour toi ! </p><br>
+                
+
+      `);
+  };
+  
+  module.exports = { Hello };
