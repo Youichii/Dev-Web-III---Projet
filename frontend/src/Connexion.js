b4253e63bdd61aa9151baff86b90941be801de4d
clementinesacre <sacre.clementine@gmail.com>
2021-05-01 09:29:24 +0200

    Message erreur lors de la conexxion si pas ok

--- frontend/src/Connexion.js
+++ frontend/src/Connexion.js
@@ -1,23 +1,52 @@
 const Connexion = () => {
 
+	const verification_valeurs = () => {
+		let compteur = true ;
+		document.getElementById("erreur_connexion").innerHTML = "";
+
+		if (document.getElementById("text_user").value === "") { 
+			document.getElementById("text_user").style.borderColor="var(--erreur)";
+			document.getElementById("erreur_mail").innerHTML = "Veuillez entrer votre adresse";
+			compteur = false ;
+		}
+		else {
+			document.getElementById("text_user").style.borderColor="var(--bordure)";
+			document.getElementById("erreur_mail").innerHTML = "";
+		}
+
+		if (document.getElementById("text_mdp").value === "") { 
+			document.getElementById("text_mdp").style.borderColor="var(--erreur)";
+			document.getElementById("erreur_mdp").innerHTML = "Veuillez entrer votre mot de passe";
+			compteur = false ;
+		}
+		else {
+			document.getElementById("text_mdp").style.borderColor="var(--bordure)";
+			document.getElementById("erreur_mdp").innerHTML = "";
+		}
+		return compteur ;
+	}
+
 	const recuperer_client = () => {
-		let mail = document.getElementById("text_user").value ;
-		let pwd = document.getElementById("text_mdp").value ;
-		var myInit = { method: 'GET',
-               headers: {'Content-Type': 'application/json'}
-        };
-        fetch(`http://localhost:3001/api/users/${mail}/${pwd}`, myInit)
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-			if (data.length === 0) {
-				console.log("data vide");
-			}
-			else {
-				console.log("data ok");
-			}
-        })
+
+		if (verification_valeurs()) {
+			let mail = document.getElementById("text_user").value ;
+			let pwd = document.getElementById("text_mdp").value ;
+			var myInit = { method: 'GET',
+				headers: {'Content-Type': 'application/json'}
+			};
+			fetch(`http://localhost:3001/api/users/${mail}/${pwd}`, myInit)
+			.then(res => {
+				return res.json();
+			})
+			.then(data => {
+				if (data.length === 0) {
+					document.getElementById("erreur_connexion").innerHTML = "Il semble que votre adresse e-mail ou votre mot de passe soient incorrects. Veuillez essayer à nouveau, s'il vous plaît";
+				}
+				else {
+					document.getElementById("erreur_connexion").innerHTML = "";
+				}
+			})
+		}
     }
 
     return (
@@ -31,17 +60,19 @@ const Connexion = () => {
 					<div className="i_titre_adresse">ADRESSE E-MAIL</div>
 					<div className="i_champ_adresse">
 						<input id="text_user" type="text" required maxLength="100" />
+						<span className="message_erreur" id="erreur_mail"></span>
 					</div>
 
 					<div className="i_titre_mdp">MOT DE PASSE</div>
 					<div className="i_champ_mdp">
 						<input id="text_mdp" type="password" required maxLength="100" />
+						<span className="message_erreur" id="erreur_mdp"></span>
 					</div>
 					
 					<div className="i_bouton_envoi">
 						<input id="bouton_connexion_envoi" type="button" value="CONNEXION" onClick={recuperer_client} />
+						<br></br><span className="message_erreur" id="erreur_connexion"></span>
 					</div>
-					<div className="i_mdp_oubli" id ="pwd_oubli">Mot de passe oublié ?</div>
 				</div>
             </div>		
         </div>
