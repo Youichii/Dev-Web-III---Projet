38097e793bedd2f2427bed7271762b82cd12064f
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-02 17:35:08 +0200

    Déplacement de fichier

---
+++ frontend/src/components/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../../App.css';
+
+import HeroSection from '../HeroSection';
+
+
+function Home() {
+  return (
+    <>
+      <HeroSection>
+        <body>
+        </body>
+      </HeroSection>
+      
+    </>
+  );
+}
+
+export default Home;
