7c491e1b5b4bc3f511d7674827b72ffbbcab667a
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-04 18:21:12 +0200

    changement de dossier

---
+++ frontend/src/Stat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../index.css';
+import Chart from './components/Chart.js';
+
+export default function Stat() {
+  return (
+    <>
+        <Chart />
+    </>
+    
+  );
+
+};
