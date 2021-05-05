583bf4c4369d444cbf8fa937ba49bf25e4a67b42
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2021-05-05 18:14:45 +0200

    Ajout contenu à la pagr Historique

---
+++ frontend/src/Historique.js
@@ -0,0 +1,57 @@
+import {useEffect, useState} from 'react';
+
+
+const Historique = () => {
+    require("./historique.css")
+    let [historique, setHistorique] = useState(null)
+    let[date, setDate] = useState([])
+
+    useEffect(()=>{
+
+        // GET qui va chercher toutes les commandes
+        var remplirHistorique = {method : 'GET',
+        headers:{'Content-type':'application/json'}
+        }
+        fetch('http://localhost:3001/historical', remplirHistorique)
+        .then(response =>{
+            return response.json()
+        })
+        .then(json =>{
+            setHistorique(json)
+        })
+
+        // GET qui va chercher toutes les commandes
+        var remplirDate = {method : 'GET',
+        headers:{'Content-type':'application/json'}
+        }
+        fetch('http://localhost:3001/historicalDate', remplirDate)
+        .then(response =>{
+            return response.json()
+        })
+        .then(json =>{
+            setHistorique(json)
+        })
+
+
+    },[])
+
+    return(
+        <div>
+        {historique&&historique.map(histo =>  
+            setDate.push(histo.DateCommande) 
+        )}
+        {date.map(dat =>
+            <div style={{color:'red'}}>{dat.DateCommande} </div>
+        )}
+        {/* {historique&&historique.map(histo => 
+            <div style={{color:'red'}}>{histo.DateCommande.slice(0,4)} </div>
+
+            )}   */}
+
+        </div>
+
+        
+
+    );
+}
+export default Historique; 
