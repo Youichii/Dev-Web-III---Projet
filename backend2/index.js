873ea425c9962ee0f0cc4f7f4b3ab6e5788ad4a3
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-06 11:07:46 +0200

    correction du transporter email

--- backend2/index.js
+++ backend2/index.js
@@ -1,25 +1,79 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-
-
-
+const bodyParser = require ('body-parser');
+const mysql = require ('mysql');
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true}));
-app.use(bodyParser.json());
-app.use(cookieParser());
+const cors = require ('cors');
+const nodemailer = require('nodemailer');
 
-const { sendEmail } = require('./mail');
+const db = mysql.createPool({
+    host: 'localhost',
+    user:'root',
+    password:'Stegosaure915',
+    database:'profilprive'
+});
+
+db.getConnection(function(err){
+    if (err) throw err;
+    console.log("db connecté")
+});
+
+app.use(express.json());
+app.use(cors());
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 
-app.post("/api/sendMail", (req, res) => {
+app.get('/api/users', (req, res)=>{
+    
+    const sqlGet = "SELECT Prenom, Mail  FROM Clients";
+    db.query(sqlGet, (err, result)=>{
+        res.send(result);
+        console.log(result)
 
-    console.log(req.body)
-    sendEmail(req.body.email, req.body.name, req.body.promo, "newsletter")
+    });
+});
 
-})
+app.listen(3000, ()=>{
+    console.log('go to localhost:3000/users')
+});
+
+//-------- Envoyer des emails ----------------------
+
+// créer le transporter et verifier s'il peut envoyer des mails 
+
+let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth:{
+        user: "nozak001@gmail.com",
+        pass:"hellodev0"
+    }
+});
+
+transporter.verify((err, success)=>{
+    err? console.log (err)
+    : console.log(`=====Pret à envoyé des mail: ${success}======`);
+});
+
+//faire passer les options au transporter et pouvoir enfaire des requêtes
+
+app.post("/envoye", function (req, res){
+    let mailOptions ={
+        from: "nozak001@gmail.com",
+        to: "nozak001@gmail.com",
+        subject: "test mail",
+        text:`${req.body.emailer.message}`
+    };
 
 
-app.listen(5000,  () => {
-    console.log( "Server Running at 5000 ");
-})
+    transporter.sendMail(mailOptions, function (err, data){
+        if (err) {
+            res.json({
+                status:"fail"
+            });
+        }else {
+            console.log ("=====Email envoyé avec succes !===== ");
+            res.json ({status: "Email envoyé"});
+
+        }
+    }); 
+});
