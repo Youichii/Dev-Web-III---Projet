3bb54f74010af0e36ac8b0fac8166389b44eb0e9
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2021-05-08 12:24:44 +0200

    stat chart retiré // fait planter le code

--- frontend/src/Stat.js
+++ frontend/src/Stat.js
@@ -2,9 +2,8 @@ import React from 'react';
 import '../index.css';
 import Chart from './Chart.js'
 //import '../../App.css';
-import Chart from '../../../../Statistique/src/components/Chart'
-import Chart from './components/Chart';
-
+// import Chart from '../../../../Statistique/src/components/Chart'
+// import Chart from './components/Chart';
 export default function Stat() {
   return (
     <>
