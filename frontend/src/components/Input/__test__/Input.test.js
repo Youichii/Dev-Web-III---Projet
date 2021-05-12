bfa2d5efdf823607ce8871e6d63d22f57c8edfbc
Youichii <baurelien@hotmail.com>
2021-05-12 10:50:09 +0200

    Adding testing for <Input /> component

--- frontend/src/components/Input/__test__/Input.test.js
+++ frontend/src/components/Input/__test__/Input.test.js
@@ -11,3 +11,19 @@ it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<Input />, div)
 })
+
+it("apply the right pattern for the input", () => {
+    const {getByTestId} = render(<Input pattern="[0-9]*/.[aAzZ]"/>);
+    expect(getByTestId('input')).toHaveAttribute("pattern" , "[0-9]*/.[aAzZ]")
+})
+
+it("Right default props : type", () => {
+    const {getByTestId} = render(<Input />);
+    expect(getByTestId('input')).toHaveAttribute("type" , "text")
+})
+
+it("Creates snapeshot with default props", () => {
+    const tree = renderer.create(<Input/>).toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
