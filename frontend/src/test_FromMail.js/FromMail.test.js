fdb6f11cf53f8847f91920e7ed978273b7726207
noellekh <63249943+noellekh@users.noreply.github.com>
2021-05-12 10:57:43 +0200

    correction

--- frontend/src/test_FromMail.js/FromMail.test.js
+++ frontend/src/test_FromMail.js/FromMail.test.js
@@ -1,4 +1,4 @@
-{/*
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import FormMail from './FormMail';
@@ -31,5 +31,3 @@ it ("form can be submittes & input fields work",()=>{
 })
 ;
 
-
-*/}
