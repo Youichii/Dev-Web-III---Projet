7f8529f345358d1762e656669960d8e33d56fbad
clementinesacre <sacre.clementine@gmail.com>
2021-05-13 17:24:24 +0200

    Correction newsletter suite au merge

--- frontend/src/FormMail.js
+++ frontend/src/FormMail.js
@@ -17,43 +17,58 @@ import './FormMail.css'
      }
 
      const SubmitEmail = async(e) =>{
-         e.preventDefault();
-         console.log({emailer});
-         const response = await fetch ("http://localhost:3000/envoye",{
-             method: "POST",
-             headers:{
-                 "Content-type": "application/json"
-             },
-             body: JSON.stringify({emailer}),
-         })
-         .then((res)=> res.json())
-         .then (()=>{
-             setEmailer({
-                 message:""
-             });
-         });
+        e.preventDefault();
+        console.log({emailer});
+        const response = await fetch ("http://localhost:3000/envoye",{
+            method: "POST",
+            headers:{
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({emailer}),
+        })
+        .then((res)=> res.json())
+        .then (()=>{
+            setEmailer({
+                message:"",
+                subject:"",
+                corps:""
+            });
+        });
      };
 
 
      return(
-         <div className="email-container">
-             <div className="formulaire-email">
+        <div className="email-container">
+        <div className="formulaire-email">
 
-                 <form data-testid="mailform" className="champ-formulaire" onSubmit={SubmitEmail}>
-                     <legend>Creer une newsletter</legend>
-                     <textarea 
-                         placeholder="Message"
-                         onChange = {handleChange}
-                         name="message"
-                         value={emailer.message}
-                     />
-                     <button className="btn-email">Envoyer email</button>
-                 </form>
+            <form data-testid="mailform" className="champ-formulaire" onSubmit={SubmitEmail}>
+                <legend>Creer une newsletter</legend>
 
-             </div>
+               <input
+                   placeholder="Sujet du message"
+                   onChange={handleChange}
+                   name="subject"
+                   value={emailer.subject}
+                />
+               <textarea 
+                   placeholder="Promo"
+                   onChange = {handleChange}
+                   name="message"
+                   value={emailer.message}
+               />
+               <textarea 
+                   placeholder="Corps"
+                   onChange = {handleChange}
+                   name="corps"
+                   value={emailer.corps}
+               />
 
+                <button className="btn-email">Envoyer email</button>
+            </form>
 
-         </div>
+        </div>
+
+    </div>
      );
  }
 
