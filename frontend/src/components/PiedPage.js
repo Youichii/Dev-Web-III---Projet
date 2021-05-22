8e02f315fb50a4df5c13d00208fb703cb2c87edf
clementinesacre <sacre.clementine@gmail.com>
2021-05-22 17:57:40 +0200

    Transfert des éléments du footer vers banner

--- frontend/src/components/PiedPage.js
+++ frontend/src/components/PiedPage.js
@@ -1,5 +1,5 @@
 import { NavLink } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 const PiedPage = () => {
 	require('../css/piedPage.css') ;
@@ -42,11 +42,6 @@ const PiedPage = () => {
                 document.getElementsByClassName(documents[4])[0].style.display = "initial";
             }
             else {
-                // document.getElementsByClassName(documents[0])[0].style.display = "initial";
-                // document.getElementsByClassName(documents[1])[0].style.display = "none";
-                // document.getElementsByClassName(documents[2])[0].style.display = "none";
-                // document.getElementsByClassName(documents[3])[0].style.display = "none";
-                // document.getElementsByClassName(documents[4])[0].style.display = "none";
                 for (let i = 0 ; i < documents.length ; i++) {
                     if (i === niveau) {
                         document.getElementsByClassName(documents[i])[0].style.display = "initial";
@@ -66,7 +61,7 @@ const PiedPage = () => {
                 <a href="https://www.privacypolicies.com/live/5b2841b7-4ad5-46a7-ac31-25ea7960e6f3" className="confidentialite">Politique de Confidentialité</a>
                 <a href="https://www.privacypolicies.com/live/544cc1d9-8359-45cf-a543-ab63691364cf" className="cookies">Politique relative aux cookies</a>
                 <NavLink to='/cgu' className="cgu">Conditions générales d'utilisation</NavLink>
-                <a className="appeler" href="">Nous appeler</a>
+                <NavLink to='/informations' className="appeler">Nous appeler</NavLink>
                 <div className="copyright">© Copyright 2021 ChickNFish</div>
                 <div className="bouton_gauche"><input type="button" value="‹" onClick={glisserGauche} /></div>
                 <div className="bouton_droite"><input type="button" value="›" onClick={glisserDroite} /></div>
